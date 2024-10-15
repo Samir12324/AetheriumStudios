42130b84f601cfe1200f4601e50169e65c9e80a4
rhithesh <hithesh@Hitheshs-MacBook-Air.local>
2024-10-15 19:27:12 +0530

    comments again

--- app/api/comment/route.ts
+++ app/api/comment/route.ts
@@ -1,22 +1,11 @@
 import prisma from "@/lib/prismaDb";
-// model Comment {
-//   id        String   @id @default(cuid())
-//   content   String
-//   createdAt DateTime @default(now())
-//   updatedAt DateTime @updatedAt
-//   authorId  String
-//   author    User     @relation(fields: [authorId], references: [id])
-//   postId    String
-//   post      Post     @relation(fields: [postId], references: [id], onDelete: Cascade)
-// }
-
 export async function POST(req, res) {
   const body = await req.json();
   const data = await prisma.comment.create({
     data: {
       content: body.comment,
       authorId: body.author.authorId,
-      postId: body.postId,
+      postId: body.postid,
     },
   });
 
